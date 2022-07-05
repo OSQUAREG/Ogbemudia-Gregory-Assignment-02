afed5c556d4eef6e0d05b84e974c67ec98b1fa81
Gregory (OSQUAREG Tech) <102813437+OSQUAREGTech@users.noreply.github.com>
2022-07-05 01:27:30 +0000

    Check Challenge 1 and 2

--- src/assignment.js
+++ src/assignment.js
@@ -12,11 +12,42 @@ const assignment = {};
  * 
  * @param {Array} arrayOfNumbers the array of numbers to sum
  * @returns number the sum of the numbers
- */
+
+
+const numbers = [
+        68,
+        -68,
+        27,
+        94,
+        72,
+        -25,
+        -51,
+        32,
+        10,
+        64,
+        -94,
+        4,
+        34,
+        -86,
+        90,
+        81,
+        20,
+        -56,
+        -91,
+        -50
+    ];
+
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    let sumNumbers = 0;
+
+    for (let i = 0; i < arrayOfNumbers.length; i++) {
+        sumNumbers += arrayOfNumbers[i];
+    }
+    return sumNumbers
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+// console.log(sumOfNumbers(numbers))
+
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -26,11 +57,38 @@ function sumOfNumbers(arrayOfNumbers) {
  * 
  * @param {Array} arrayOfNumbers the array containing even or non-even numbers
  * @returns number the count of even numbers
- */
+
+
+const numbers = [
+        17,
+        0,
+        67,
+        41,
+        49,
+        21,
+        1,
+        86,
+        76,
+        2,
+        54,
+        14,
+        93,
+        84,
+        14,
+        77,
+        35,
+        7,
+        29,
+        64
+    ];
+
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    let evenNumbers = arrayOfNumbers.filter(number => number % 2 == 0)
+    return evenNumbers.length;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+// console.log(countEvenNumbers(numbers))
+
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
